50257c443fcfa1d92278cc095260eeaed06522a0
m4t1ss3 <mark@orart.de>
2025-04-26 22:00:02 +0200

    refactor: clean up package.json and globals.css, remove unused dependencies and styles

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -13,11 +13,6 @@ module.exports = {
           dark: '#003772',
         },
       },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
     },
   },
   plugins: [],
