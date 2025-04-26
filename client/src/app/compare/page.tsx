49593306b1ad588eaa4d81591f704542ee8819f3
m4t1ss3 <mark@orart.de>
2025-04-26 22:54:29 +0200

    feat: add ComparePage component for comparison functionality

---
+++ client/src/app/compare/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+'use client';
+
+
+const ComparePage = () => {
+    return (
+        <div className="container mx-auto p-4">
+            <h1 className="text-2xl font-bold mb-4">Compare</h1>
+            <div>
+                {/* Add your comparison content here */}
+            </div>
+        </div>
+    );
+};
+
+export default ComparePage;
